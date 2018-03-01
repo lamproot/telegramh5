bd217be9c42a3494ecbd946b4d487178fde97903
quanboxiong <quanboxiong@6clue.com>
2018-03-01 17:00:23 +0800

    更新

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ error_reporting(-1);                    //打印出所有的 错误信息
 
 define('APP_NAME','index');
 	define('APP_PATH','./index/');
-	define('APP_DEBUG',false);
+	define('APP_DEBUG',true);
 	echo "1212";
 	require './ThinkPHP/ThinkPHP.php';
 ?>
