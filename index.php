51372dc18e020fe283450b3e1036142f52533071
quanboxiong <quanboxiong@6clue.com>
2018-03-01 16:41:14 +0800

    更新本地

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ ini_set('display_startup_errors',1);    //php启动错误信息
 error_reporting(-1);                    //打印出所有的 错误信息
 
 define('APP_NAME','index');
-	define('APP_NAME','index');
 	define('APP_PATH','./index/');
 	define('APP_DEBUG',false);
 	echo "1212";
