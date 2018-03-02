e067dce66142028d35c02baa484609005aa3ef5e
quanboxiong <quanboxiong@6clue.com>
2018-03-02 11:22:25 +0800

    更新

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
-ini_set('display_errors',1);            //错误信息
-ini_set('display_startup_errors',1);    //php启动错误信息
-error_reporting(-1);                    //打印出所有的 错误信息
-
-define('APP_NAME','index');
+	define('APP_NAME','index');
 	define('APP_PATH','./index/');
 	define('APP_DEBUG',false);
-
-	echo "1212";
-	print_r($_SERVER) ;
 	require './ThinkPHP/ThinkPHP.php';
 	// define('ROOT',$_SERVER['DOCUMENT_ROOT']);
 	// define('THINK_PATH',ROOT.'/ThinkPHP/');
