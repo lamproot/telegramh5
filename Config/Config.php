280d34375a2e2bfd8450c04784cf38578461970f
quanboxiong <quanboxiong@6clue.com>
2018-03-02 11:18:16 +0800

    更新

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+    define ('DBHOST', '127.0.0.1');
+    define ('DBNAME', 'telegram');
+    define ('DBUSER', 'root');
+    define ('DBPASS', 'root');
+    define ('DBPERSISTENT', false);
+    // define ('DBHOST', '18.218.250.89');
+    // define ('DBNAME', 'telegram');
+    // define ('DBUSER', 'telegram');
+    // define ('DBPASS', 'telegram');
+    // define ('DBPERSISTENT', false);
