902bda594f2a01046f55570eea36352fa1e72a13
quanboxiong <quanboxiong@6clue.com>
2018-03-04 15:33:30 +0800

    更新

--- Plugins/Basic/Basic.class.php
+++ Plugins/Basic/Basic.class.php
@@ -1,17 +1,85 @@
 <?php
-class Basic extends Base {
-    public function command ($command, $param, $message_id, $from, $chat, $date) {
-        if ($command == '/me' || $command == '/my' ) {
-            $code = "xxxxxxxxXXXXXXX";
-            $message = "Your code: {$code}. SUCCESS & DONE! For every friend you invite from now on, you will get 188 SSS from our Airdrop event within a month
-你的驗證碼：{$code}，校驗成功！每邀请一个好友，你将在 1 個月内收到 188 SSS 的空投獎勵。
-Your share link （你的分享鏈接）：http://www.sss.run/?code={$code}";
-            $this->telegram->sendMessage (
-                $chat['id'],
-                $message,
-                $message_id
-            );
+    class Basic extends Base {
+        public function command ($command, $param, $message_id, $from, $chat, $date) {
+            # 步骤
+            //1.查询是否以 / 开头 正则匹配
+            //$search = "/^\//i"; preg_match($search,$command,$result)
+            if($command[0] == "/") {
+                $errorModel = new errorModel();
+                            $errorModel->sendError (MASTER, print_r($command, true));
+                $chatBotModel = new chatBotModel;
+                $chatBot = $chatBotModel->getcommand($chat['id']);
+                $chat_bot_id = ($chatBot && isset($chatBot['id'])) ? $chatBot['id'] : "";
+
+                //查询命令是否有回复
+                $commandModel = new commandModel;
+                $commandInfo = $commandModel->findall($chat_bot_id, $command, 1, 1);
+
+                if ($commandInfo && $commandInfo[0] && $commandInfo[0]['type']) {
+
+                    //type =  1 文字回复  2 code 码回复 3 图片文字回复 4 文件回复
+                    if ($commandInfo[0]['type'] == 1) {
+                        $message = ($commandInfo && $commandInfo[0] && isset($commandInfo[0]['content']) && !empty($commandInfo[0]['content'])) ? $commandInfo[0]['content'] : "";
+                        if ($message) {
+                            $this->telegram->sendMessage (
+                                $chat['id'],
+                                $message,
+                                $message_id
+                            );
+                        }
+                    }
+
+                    if ($commandInfo[0]['type'] == 3) {
+                        $copyright = $commandInfo[0]['content'] ? $commandInfo[0]['content'] : "";
+                        $url = $commandInfo[0]['url'] ? $commandInfo[0]['url'] : "";
+                        if ($url) {
+                            $this->telegram->sendPhoto ($chat['id'], $url, $copyright, $message_id);
+                        }
+                    }
+
+                    if ($commandInfo[0]['type'] == 4) {
+                        $copyright = $commandInfo[0]['content'] ? $commandInfo[0]['content'] : "";
+                        $url = $commandInfo[0]['url'] ? $commandInfo[0]['url'] : "";
+                        if ($url) {
+                            $this->telegram->sendDocument ($chat['id'], $url, $copyright, $message_id);
+                        }
+                    }
+
+                }
+            }
         }
 
+        public function new_member ($new_member, $message_id, $from, $chat, $date) {
+            // $command = "new_member";
+            // //创建欢迎消息
+            // $commandModel = new commandModel;
+            // $commandInfo = $commandModel->find($chat['id'], $command, 1, 1);
+            //
+            // //消息调试
+            // // $errorModel = new ErrorModel;
+            // // $errorModel->sendError (MASTER, $commandInfo[0]['content']);
+            // //type =  1 文字回复  2 code 码回复 3 图片文字回复
+            // $message = ($commandInfo && $commandInfo[0] && isset($commandInfo[0]['content']) && !empty($commandInfo[0]['content'])) ? $commandInfo[0]['content'] : "";
+            // if ($message) {
+            //     $this->telegram->sendMessage (
+            //         $chat['id'],
+            //         $message,
+            //         $message_id
+            //     );
+            // }
+            // $str = '@' . @$from['username'] . ' 邀请了 @' . $new_member['username'] . ' 来到 ' . $chat['title'] . ' 玩' . "\n";
+            // $str .= '欢迎 @' . $new_member['username'] . ' 来到 ' . $chat['title'] . '  玩(ฅ>ω<*ฅ)';
+            // $this->telegram->sendMessage ($chat['id'], $str, $message_id, array (), '');
+        }
+        public function left_member ($left_member, $message_id, $from, $chat, $date) {
+            // $str = '喵喵喵？ @' . $left_member['username'] . ' 被 @' . @$from['username'] . ' 移出了 ' . $chat['title'];
+            // $this->telegram->sendMessage ($chat['id'], $str, $message_id, array (), '');
+            $chatBotModel = new chatBotModel;
+            $chatBot = $chatBotModel->getcommand($chat['id']);
+            $chat_bot_id = ($chatBot && isset($chatBot['id'])) ? $chatBot['id'] : "";
+
+            $codeModel = new codeModel;
+            $codeModel->updateByFromId($chat_bot_id, @$from['id']);
+
+        }
     }
-}
