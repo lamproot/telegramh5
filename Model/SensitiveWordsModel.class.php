554d9e57cfa52b2cd8969d268b2df3a154b57daf
lamproot <304151978@qq.com>
2018-03-11 22:21:38 +0800

    更新

--- Model/SensitiveWordsModel.class.php
+++ Model/SensitiveWordsModel.class.php
@@ -3,14 +3,15 @@
         function find ($word, $limit = 0)
         {
             /** 查询 */
-            $where = array (
-                'ORDER' => 'id'
-            );
+            $where = array ();
             $word === NULL ? : $where['AND']['word'] = $word;
 
             if ($limit != 0) {
                 $where['LIMIT'] = $limit;
             }
+            $errorModel = new ErrorModel;
+            $errorModel->sendError (MASTER, print_r($where, true));
+                        
             $ret = $this->db->select ('app_sensitive_words', '*', $where);
             return $ret;
         }
