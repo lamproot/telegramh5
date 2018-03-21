a900df72e2823a263365ec8ce570b5365b82c55a
quanboxiong <quanboxiong@6clue.com>
2018-03-21 18:07:46 +0800

    更新

--- Plugins/Code/Code.class.php
+++ Plugins/Code/Code.class.php
@@ -44,10 +44,21 @@
                 $message = "";
 
                 $codeModel = new CodeModel;
+                //查询该用户是否已激活
+                $codeInfoByUid = $codeModel->getCodeByFromId($chat_bot_id, $from['id']);
+                if ($codeInfoByUid) {
+                    $codeLogModel = new CodeLogModel;
+                    $username = isset($from['username']) ? $from['username'] : "";
+                    $first_name = isset($from['first_name']) ? $from['first_name'] : "";
+                    $last_name = isset($from['last_name']) ? $from['last_name'] : "";
+
+                    $codeLogModel->add($chat_bot_id, $message_id, $code, @$message, @$from['id'], @$username, $first_name, $last_name);
+                    return true;
+                }
+
                 $codeInfo = $codeModel->find($chat_bot_id, $code, 1);
 
                 //用户码 进行用户绑定 from_id 对应
-
                 //用户码记录表 chat_id-群ID  from_id-来源ID  eth-用户钱包 code-我的 parent_code-父(传播者 邀请者)
                 //status-用户状态 默认 0 1-已申请用户码 2-已在群里确认用户码(已在群里) -1-已退出群聊
                 //code 码规则 用户钱包地址_群ID_telegram_code_2018
@@ -55,7 +66,7 @@
 
                 // $errorModel = new ErrorModel;
                 // $errorModel->sendError (MASTER, 'from'. print_r($from, true));
-
+                //查询该用户是否已激活
                 if ($codeInfo && $codeInfo[0]) {
                     if ($commandFind && $commandFind[0] && $commandFind[0]['content']) {
                         $message = str_replace("{{".$code_cmd."}}", $code, $commandFind[0]['content']);
