85a92944de715584dda20aa66eb2eb08e95f31c7
lamproot <304151978@qq.com>
2018-03-21 22:36:55 +0800

    gengxin

--- Config/Config.php
+++ Config/Config.php
@@ -3,7 +3,7 @@
     define ('DBNAME', 'telegram');
     define ('DBUSER', 'root');
     define ('DBPASS', 'root');
-    define ('DBPERSISTENT', false);
+    define ('DBPERSISTENT', true);
     define ('DBCHARSET', 'utf8mb4');
     // define ('DBHOST', '18.218.250.89');
     // define ('DBNAME', 'telegram');
