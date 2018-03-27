e8aa80c097288b31b18aa19d62056bb1f6d31e0a
quanboxiong <quanboxiong@6clue.com>
2018-03-27 14:18:14 +0800

    更新

--- Config/BotConfig.php
+++ Config/BotConfig.php
@@ -1,6 +1,6 @@
 <?php
     define ('BOTNAME', 'Testxiaoxiong_bot');
-    define ('TOKEN', '525776656:AAEyenE0yMyClFuzsiFm4UMNFolJJOP04j0');
+    define ('TOKEN', '525776656:AAHGITDkvLnzyPD89AupHnpD1F8TPERIeRQ');
     define ('MASTER', '520439801');
     define ('FASTLOGIN', false);
     define ('DEBUG', true);
