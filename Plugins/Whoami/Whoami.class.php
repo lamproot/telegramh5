1ef228f8abadca9ddc34770d193ae60ecb4d7436
quanboxiong <quanboxiong@6clue.com>
2018-04-04 16:48:11 +0800

    更新

--- Plugins/Whoami/Whoami.class.php
+++ Plugins/Whoami/Whoami.class.php
@@ -10,6 +10,16 @@
             //     $this->token
             // }
 
+            //激活机器人 输入激活码  查询机器人是否已激活 已激活-返回已激活 未激活返回未激活状态
+            // 是否激活  is_activate  是否工作 is_work 是否工作
+            if ($command == '/activatebot') {
+                $str = "已激活";
+                $str .= '你的 master_id：' . $from['id'] . "\n";
+                $str .= '群的 chat_id：' . $chat['id'] . "\n";
+                $str .= '这条消息的 id：' . $message_id . "\n";
+                $this->telegram->sendMessage (MASTER, $str, $message_id);
+            }
+
             $search = "/^\/whoami/i";
 
             if(preg_match($search,$command,$result)) {
