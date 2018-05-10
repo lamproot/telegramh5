1daf40871952487e72d7a4fd8bbaa76cab246b77
quanboxiong <quanboxiong@6clue.com>
2018-05-10 14:53:13 +0800

    更新

--- Plugins/Whoami/Whoami.class.php
+++ Plugins/Whoami/Whoami.class.php
@@ -2,20 +2,20 @@
     class Whoami extends Base {
         public function command ($command, $param, $message_id, $from, $chat, $date) {
             if ($command == '/whoami') {
-                $chat_id = $_GET['bot_id'] ? intval($_GET['bot_id']) : $chat['id'];
+                // $chat_id = $_GET['bot_id'] ? intval($_GET['bot_id']) : $chat['id'];
 
-                if (isset($_GET['bot_id'])) {
-                    $chatBot = $chatBotModel->getById($chat_id);
-                }else{
-                    $chatBot = $chatBotModel->getByChatId($chat_id);
-                }
+                // if (isset($_GET['bot_id'])) {
+                //     $chatBot = $chatBotModel->getById($chat_id);
+                // }else{
+                //     $chatBot = $chatBotModel->getByChatId($chat_id);
+                // }
 
-                if ($chatBot) {
-                    $_SESSION['token'] = $chatBot['token'];
-                }
-                
-                $str = '你的 master_id：' . print_r($from, true) . "\n";
-                $str .= '群的 chat_id：' . print_r($chat, true) . "\n";
+                // if ($chatBot) {
+                //     $_SESSION['token'] = $chatBot['token'];
+                // }
+
+                $str = '你的 master_id：' . $from['id']. "\n";
+                $str .= '群的 chat_id：' . $chat['id'] . "\n";
                 $str .= '这条消息的 id：' . $message_id . "\n";
                 $this->telegram->sendMessage (MASTER, $str, $message_id);
             }
