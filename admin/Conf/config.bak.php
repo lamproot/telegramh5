09b429a6cf28d40a97d3916a2c4da01d2f4c152a
lamproot <304151978@qq.com>
2018-05-12 17:33:40 +0800

    更新

--- admin/Conf/config.bak.php
+++ admin/Conf/config.bak.php
@@ -11,10 +11,7 @@ return array(
 	'DB_NAME'=>'telegram',
 	'APP_DEBUG' => false,
 	'SHOW_PAGE_TRACE'=>false,
-	'DB_CHARSET'=> 'utf8mb4',
-	'view_replace_str'       => [
-        '__PUBLIC__'=> SCRIPT_DIR . '/public'              //定义首页
-    ],
+	'DB_CHARSET'=> 'utf8mb4'
 );
 /*
 return array(
