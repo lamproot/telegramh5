181377d962d96dd73806c21b326e709187eb8a7a
lamproot <304151978@qq.com>
2018-05-15 23:57:23 +0800

    gengxin

--- Plugins/Basic/Basic.class.php
+++ Plugins/Basic/Basic.class.php
@@ -149,17 +149,9 @@
 
                 //链接  关键字（敏感词）过滤
                 $regex = '@(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))@';
-                $needle= ['http','join','Join'];
-
-                $blank = false;
-                foreach ($needle as $key => $value) {
-                    $pos = strripos(strtolower($message), $value);
-                    if ($pos !== false) {
-                        $blank = true;
-                    }
-                }
-
-                if (preg_match($regex, $message) || $blank !== false) {
+                $needle= 'http';
+                $pos = strripos($message, $needle);
+                if (preg_match($regex, $message) || $pos !== false) {
                     $sendmessage = "Opps... error！Any ads posted in here are not allowed ，such as profiles，links，pictures etc... They will be automatically deleted. Please don't send these contents any more，or you will be taken out of the group.";
 
                     $IllegalLogModel = new IllegalLogModel;
